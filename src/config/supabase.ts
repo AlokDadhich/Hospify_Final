3655086b7e697e7066a9a850403c469b61bea519
AlokDadhich <139156911+AlokDadhich@users.noreply.github.com>
2025-06-29 13:55:05 +0530

    Fix Supabase Configuration

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,11 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
+const supabaseUrl = 'https://vtcycnnfyxlausxylxtq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0Y3ljbm5meXhsYXVzeHlseHRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExODAzNDAsImV4cCI6MjA2Njc1NjM0MH0.y88jQ-C2Y0TJaLK2iV57xk9ESiOhanwB1EW0bzQkGfg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
